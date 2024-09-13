bac3529f03d2ba2b13f27db9cb1469605a93856e
Swekit Patel <161512200+swekizer@users.noreply.github.com>
2024-09-13 19:01:04 +0000

    String

--- 01_basics/07_String.js
+++ 01_basics/07_String.js
@@ -7,14 +7,44 @@ console.log(name +  " " + "and" + "age is" + " " + age);
 
 console.log(`My name is ${name} and my age is ${age}`);
 
-const name1 = new String ('Rudra')
+const name1 = new String ('Rudra-hello')
 
 console.log(name1);
 
 console.log(name1[0]);
+
+//String Methods
 console.log(name1.length);
 console.log(name1.toUpperCase());
 console.log(name1.__proto__);
+
+console.log(name1.charAt(2));
+console.log(name1.indexOf('r'));
+
+const newString = name1.substring(0,4)
+console.log(newString);
+
+const newString1 = name1.slice(-4,4)
+console.log(newString1);
+
+const newString2 = "    Swekit    "
+console.log(newString2);
+console.log(newString2.trim());
+
+const url = "https://www.google.com/leetcode%20swekizer"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('google'));
+console.log(url.includes('swekit'));
+
+console.log(name1.split('-'));
+
+console.log();
+
+
+
+
+
 
 
 
